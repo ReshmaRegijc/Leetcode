4b03154793dea327e4f086c8c6f89825beff96ad
ReshmaRegijc <90573572+ReshmaRegijc@users.noreply.github.com>
2021-09-13 02:02:31 +0530

    Update Implement_strStr.py

--- Implement_strStr.py
+++ Implement_strStr.py
@@ -2,5 +2,5 @@ def strStr(self, haystack: str, needle: str) -> int:
     if len(needle)==0:
         return 0
     x = haystack.find(needle)
-    print(x)
     return x
+print(strStr("hello", "ll")==2)
