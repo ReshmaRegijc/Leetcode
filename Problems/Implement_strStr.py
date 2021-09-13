4d5493fa08347f3def4702f8ce5d445f9f7a1daa
ReshmaRegijc <reshmaregi.jc@gmail.com>
2021-09-13 21:21:20 +0530

    Updated

--- Problems/Implement_strStr.py
+++
@@ -1,6 +0,0 @@
-def strStr(self, haystack: str, needle: str) -> int:
-    if len(needle)==0:
-        return 0
-    x = haystack.find(needle)
-    return x
-print(strStr("hello", "ll")==2)
---
+++ tests/test_problem.py
@@ -0,0 +1,7 @@
+from Length_of_Last_Word import lengthOfLastWord
+from Implement_strStr import strStr
+
+def lengthOfLastWord():
+    assert lengthOfLastWord('Hi, Welcome to Leetcode')==8
+def strStr():
+    assert strStr("hello", "ll")==2
